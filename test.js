a2aa4144438024111a3789aba1b70619b188ec90
Alexandre Callens <alekzxand@gmail.com>
2019-02-14 15:20:01 +0100

    mul

--- test.js
+++ test.js
@@ -1,30 +1,7 @@
-// function coucou(guy) {
-//     return `hello ${guy}`;
-// };
-const coucou = (guy) => {
-    return `hello ${guy}`;
-}
-let counter = 5;
-const table = ['Michel', 'Tristan', 'Alex', 'Oreo'];
-const objectTable = [
-    {
-        name: 'Michel',
-        old: 35,
-        gender: 'Male'
-    },
-    {
-        name: 'Tristant',
-        old: 29,
-        gender: 'Male'
-    },
-    {
-        name: 'Tristant',
-        old: 29,
-        gender: 'Male'
-    }
-];
+const multiplicator = (number, multiple) => {
+    return number * multiple;
+};
 
-objectTable.forEach(function (obj) {
-    console.log(coucou(obj.old))
-})
+const result = multiplicator(32, 10);
 
+console.log(result);
