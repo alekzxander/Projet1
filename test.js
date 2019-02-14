46970fcaee7d3e591c0b5ad35b253fc0a6c44390
Florkin <tristan.florin@gmail.com>
2019-02-14 15:11:32 +0100

    ajout fonction

---
+++ test.js
@@ -0,0 +1,12 @@
+function coucou(name){
+    return `Hello ${name}`;
+};
+
+coucou('Alex');
+
+let count = 5;
+
+while(count <= 10){
+    console.log(coucou('Chell') +' ' + count + ' fois');
+    count ++;
+}ù
