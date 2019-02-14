7d1a367e3b603bacf35596b71bd971ccddf605d2
Alexandre Callens <alekzxand@gmail.com>
2019-02-14 15:24:05 +0100

    mm

--- test.js
+++ test.js
@@ -1,3 +1,32 @@
+
+const coucou = (guy) => {
+    return `hello ${guy}`;
+}
+let counter = 5;
+const table = ['Michel', 'Tristan', 'Alex', 'Oreo'];
+const objectTable = [
+    {
+        name: 'Michel',
+        old: 35,
+        gender: 'Male'
+    },
+    {
+        name: 'Tristant',
+        old: 29,
+        gender: 'Male'
+    },
+    {
+        name: 'Tristant',
+        old: 29,
+        gender: 'Male'
+    }
+];
+
+objectTable.forEach(function (obj) {
+    console.log(coucou(obj.old))
+})
+
+
 const multiplicator = (number, multiple) => {
     return number * multiple;
 };
